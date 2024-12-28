50eae80380cf71a112df49c8704ca0c2aea799d1
Abhi G <abhijeet@mintix.no>
2024-12-28 15:24:41 +0530

    blog section styling

--- scripts/index.js
+++ scripts/index.js
@@ -30,15 +30,6 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    // Track clicks on team member LinkedIn buttons
-    const linkedinButtons = document.querySelectorAll('.team .btn');
-    linkedinButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const memberName = this.closest('.card').querySelector('.card-title').textContent;
-            _gs('event', 'LinkedIn Click', { member: memberName });
-        });
-    });
-
     function domainExperts() {
         // Change domain-experts text every 5 seconds
         const domainExpertsElement = document.getElementById("domain-experts");
@@ -53,88 +44,81 @@ document.addEventListener("DOMContentLoaded", function() {
         setInterval(updateDomainExpertText, 5000); // Change text every 5 seconds
     }
 
-    async function fetchBlogPosts(page = 1, limit = 3) {
+    function scrollCarousel(container, direction) {
+        const itemWidth = container.querySelector('.carousel-item').offsetWidth;
+        container.scrollBy({
+            left: direction * itemWidth,
+            behavior: 'smooth'
+        });
+    }
+
+    function displayBlogPosts(posts) {
+        const blogSection = document.getElementById('blog-posts');
+        blogSection.innerHTML = ''; // Clear existing posts
+
+        const chunkSize = 3; // Number of posts to show at a time
+        for (let i = 0; i < posts.length; i += chunkSize) {
+            const chunk = posts.slice(i, i + chunkSize);
+            const carouselItem = document.createElement('div');
+            carouselItem.className = `carousel-item ${i === 0 ? 'active' : ''}`;
+
+            const row = document.createElement('div');
+            row.className = 'row';
+
+            chunk.forEach(post => {
+                const col = document.createElement('div');
+                col.className = 'col-md-4';
+                col.innerHTML = `
+                    <a href="${post.url}" class="text-decoration-none">
+                        <div class="card h-100">
+                            <img src="${post.feature_image}" class="card-img-top" alt="${post.title}">
+                            <div class="card-body">
+                                <h5 class="card-title">${post.title}</h5>
+                            </div>
+                        </div>
+                    </a>
+                `;
+                row.appendChild(col);
+            });
+
+            carouselItem.appendChild(row);
+            blogSection.appendChild(carouselItem);
+        }
+
+        // Add navigation buttons
+        const prevBtn = document.createElement('button');
+        prevBtn.innerHTML = '<i class="bi bi-arrow-left-short" />';
+        prevBtn.classList.add('btn', 'btn-icon-circle');
+        prevBtn.addEventListener('click', () => scrollCarousel(carouselContainer, -1));
+
+        const nextBtn = document.createElement('button');
+        nextBtn.innerHTML = '<i class="bi bi-arrow-right-short" />';
+        nextBtn.classList.add('btn', 'btn-icon-circle', 'ms-2');
+        nextBtn.addEventListener('click', () => scrollCarousel(carouselContainer, 1));
+
+        const paginationControls = document.getElementById('pagination-controls');
+        paginationControls.innerHTML = '<div class="col-md-10 offset-md-1 text-center"><a href="./blog" class="view-all">View All</a></div>';
+        const btnControls = document.createElement('div');
+        btnControls.classList.add('col-md-1', 'd-flex', 'align-right');
+        btnControls.appendChild(prevBtn);
+        btnControls.appendChild(nextBtn);
+        paginationControls.appendChild(btnControls);
+    }
+
+    async function fetchBlogPosts() {
         const ghostApiUrl = 'https://www.ninjamonk.in/ghost/api/content/posts/';
         const apiKey = '4d87e10fa3f68a7d4dc524e491'; // Replace with your actual API key
         try {
             const response = await fetch(`${ghostApiUrl}?key=${apiKey}&limit=all&filter=tag:-clients`);
             const data = await response.json();
-            const shuffledPosts = shuffleArray(data.posts).slice(0, limit);
-            displayBlogPosts(shuffledPosts, page, limit, Math.ceil(data.posts.length / limit));
+            displayBlogPosts(data.posts);
         } catch (error) {
             console.error('Error fetching blog posts:', error);
         }
     }
 
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
-    function displayBlogPosts(posts, currentPage, limit, totalPages) {
-        const blogSection = document.getElementById('blog-posts');
-        blogSection.innerHTML = ''; // Clear existing posts
-        posts.forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.className = 'col-md-4';
-            postElement.innerHTML = `
-                <div class="card h-100 shadow-sm">
-                    <img src="${post.feature_image}" class="card-img-top" alt="${post.title}">
-                    <div class="card-body">
-                        <h5 class="card-title">${post.title}</h5>
-                        <p class="card-text">${post.excerpt}</p>
-                        <a href="${post.url}" target="_blank" class="btn btn-outline-danger">Read More</a>
-                    </div>
-                </div>
-            `;
-            blogSection.appendChild(postElement);
-        });
-
-        // Add pagination controls
-        const paginationControls = document.getElementById('pagination-controls');
-        paginationControls.innerHTML = ''; // Clear existing controls
-
-        if (currentPage > 1) {
-            const firstButton = document.createElement('button');
-            firstButton.className = 'btn btn-secondary me-2';
-            firstButton.innerHTML = '<i class="fas fa-angle-double-left"></i>'; // First icon
-            firstButton.onclick = () => fetchBlogPosts(1, limit);
-            paginationControls.appendChild(firstButton);
-
-            const prevButton = document.createElement('button');
-            prevButton.className = 'btn btn-secondary me-2';
-            prevButton.innerHTML = '<i class="fas fa-angle-left"></i>'; // Previous icon
-            prevButton.onclick = () => fetchBlogPosts(currentPage - 1, limit);
-            paginationControls.appendChild(prevButton);
-        }
-
-        for (let i = 1; i <= totalPages; i++) {
-            const pageButton = document.createElement('button');
-            pageButton.className = `btn ${i === currentPage ? 'btn-primary' : 'btn-secondary'} me-2`;
-            pageButton.textContent = i;
-            pageButton.onclick = () => fetchBlogPosts(i, limit);
-            paginationControls.appendChild(pageButton);
-        }
-
-        if (currentPage < totalPages) {
-            const nextButton = document.createElement('button');
-            nextButton.className = 'btn btn-secondary me-2';
-            nextButton.innerHTML = '<i class="fas fa-angle-right"></i>'; // Next icon
-            nextButton.onclick = () => fetchBlogPosts(currentPage + 1, limit);
-            paginationControls.appendChild(nextButton);
-
-            const lastButton = document.createElement('button');
-            lastButton.className = 'btn btn-secondary';
-            lastButton.innerHTML = '<i class="fas fa-angle-double-right"></i>'; // Last icon
-            lastButton.onclick = () => fetchBlogPosts(totalPages, limit);
-            paginationControls.appendChild(lastButton);
-        }
-    }
-
-    fetchBlogPosts(); // Initial call to fetch and display the first page of blog posts
+    //blogCarousel();
+    fetchBlogPosts(); // Initial call to fetch and display the blog posts
     portfolioGallery(); // Initial call to set up the gallery
     domainExperts();
 });
